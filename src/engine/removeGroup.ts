a78f97b81dcf64940db1be4fd38830cab38b833a
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-07-07 15:51:27 -0300

    feat: add group removal engine

---
+++ src/engine/removeGroup.ts
@@ -0,0 +1,14 @@
+import type { Board, StoneGroup } from "../types/game";
+
+export function removeGroup(board: Board, group: StoneGroup): Board {
+  const nextBoard = structuredClone(board) as Board;
+
+  for (const stone of group.stones) {
+    nextBoard[stone.y][stone.x] = {
+      position: stone,
+      state: "EMPTY"
+    };
+  }
+
+  return nextBoard;
+}
