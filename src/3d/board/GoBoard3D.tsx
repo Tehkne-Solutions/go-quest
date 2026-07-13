ea011c8ca9cce8b68db2807da9aa38429711969b
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-07-13 13:32:54 -0300

    feat: render board FX layer in 3D scene

--- src/3d/board/GoBoard3D.tsx
+++ src/3d/board/GoBoard3D.tsx
@@ -3,5 +3,39 @@ import { BoardIntersection3D } from "./BoardIntersections";
 import { useBoard3DPositions } from "../hooks/useBoard3DPositions";
 import type { Board3DProps } from "../types/render3d";
 import { BoardPieces3D } from "../pieces/BoardPieces3D";
-function samePosition(a?:{x:number;y:number},b?:{x:number;y:number}){return Boolean(a&&b&&a.x===b.x&&a.y===b.y)}
-export function GoBoard3D({board,expectedMove,selectedPosition,showTarget,onIntersectionClick}:Board3DProps){const positions=useBoard3DPositions();return <group><BoardGround/>{positions.map(({board:position,world})=><BoardIntersection3D key={`intersection-${position.x}-${position.y}`} position={position} worldPosition={world} isExpected={showTarget&&samePosition(position,expectedMove)} isSelected={samePosition(position,selectedPosition)} onClick={onIntersectionClick}/>) }<BoardPieces3D board={board} selectedPosition={selectedPosition}/></group>}
+import { BoardFx3D } from "../fx/BoardFx3D";
+
+function samePosition(a?: { x: number; y: number }, b?: { x: number; y: number }) {
+  return Boolean(a && b && a.x === b.x && a.y === b.y);
+}
+
+export function GoBoard3D({
+  board,
+  expectedMove,
+  selectedPosition,
+  showTarget,
+  fxEvents = [],
+  onIntersectionClick
+}: Board3DProps) {
+  const positions = useBoard3DPositions();
+
+  return (
+    <group>
+      <BoardGround />
+
+      {positions.map(({ board: position, world }) => (
+        <BoardIntersection3D
+          key={`intersection-${position.x}-${position.y}`}
+          position={position}
+          worldPosition={world}
+          isExpected={showTarget && samePosition(position, expectedMove)}
+          isSelected={samePosition(position, selectedPosition)}
+          onClick={onIntersectionClick}
+        />
+      ))}
+
+      <BoardPieces3D board={board} selectedPosition={selectedPosition} />
+      <BoardFx3D events={fxEvents} />
+    </group>
+  );
+}
